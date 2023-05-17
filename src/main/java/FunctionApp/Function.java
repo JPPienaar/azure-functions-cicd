c3347221c6fb451788a33dc846a9f64eed9ec21f
JPPienaar <jp.pienaar7@gmail.com>
2023-05-17 14:22:58 +0200

    test deployment

--- src/main/java/FunctionApp/Function.java
+++ src/main/java/FunctionApp/Function.java
@@ -22,7 +22,7 @@ public class Function {
      * 1. curl -d "HTTP Body" {your host}/api/HttpExample
      * 2. curl "{your host}/api/HttpExample?name=HTTP%20Query"
      */
-    @FunctionName("HttpExample")
+    @FunctionName("Function")
     public HttpResponseMessage run(
             @HttpTrigger(
                 name = "req",
